6cb613aa7c6737c1cc6aa1be6dd181ff1eb3c86e
EC2 Default User <ec2-user@ip-172-31-2-109.ec2.internal>
2021-01-20 18:36:44 +0000

    incl.reduced echo

--- function.sh
+++ function.sh
@@ -1,9 +1,6 @@
 function handler () {
 	  EVENT_DATA=$1
 	    echo "$EVENT_DATA" 1>&2;
-	      RESPONSE="Echoing request: '$EVENT_DATA'"
-
-	        echo $RESPONSE
 		cd /tmp	
 		curl -O ${DEPLOYMENT_FILE}
 		
